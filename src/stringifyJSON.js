6df5da9102d82f1ecf9a6d19402587e068301887
kuzhi23 <9size.marvel@gmail.com>
2021-04-23 16:54:53 +0900

    stringify study

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -26,10 +26,37 @@
  * 하지만 이 과제의 목적은 재귀를 공부하는 것이니, 처음부터 구현해봐야겠지요?:
  */
 function stringifyJSON(obj) {
-  // your code goes here
-};
-
-
+  if (obj === null) {
+    return "null";
+  }
+  if (typeof obj === "number" || typeof obj === "boolean") {
+    return String(obj);
+  }
+  if (typeof obj === "string") {
+    return `"${obj}"`;
+  }
+  //obj가 배열인 경우
+  if (Array.isArray(obj)) {
+    const result = [];
+    obj.forEach(function (element) {
+      result.push(stringifyJSON(element));
+    });
+    return `[${result}]`;
+  }
+  //obj가 객체인 경우 (객체에는 배열, 객체 모두 포함되지만 배열인 경우보다 아래에 작성했으므로 배열은 제외된다.)
+  if (typeof obj === "object") {
+    let result = "";
+    for (let key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === "function") {
+        result = String(result);
+      } else {
+        result = result + `${stringifyJSON(key)}:${stringifyJSON(obj[key])},`;
+      }
+    }
+    result = result.substr(0, result.length - 1);
+    return `{${result}}`;
+  }
+}
 
 // 다음 코드는 결과 제출을 위한 코드입니다. 신경쓰지 않아도 좋습니다.
 if (typeof window === "undefined") {
